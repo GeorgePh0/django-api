7ede63a96b64f12c1f2c09a24a345e92169c4956
GeorgePh0 <george.philip0020@gmail.com>
2023-12-24 13:26:31 +0000

    Profile filter challenge

--- profiles/views.py
+++ profiles/views.py
@@ -23,12 +23,13 @@ class ProfileList(generics.ListAPIView):
     ]
     filterset_fields = [
         'owner__following__followed__profile',
+        'owner__followed__owner__profile',
     ]
     ordering_fields = [
         'posts_count',
         'followers_count',
         'following_count',
-        'owner,__following__created_at',
+        'owner__following__created_at',
         'owner__followed__created_at',
     ]
 
@@ -39,8 +40,8 @@ class ProfileDetail(generics.RetrieveUpdateAPIView):
     """
     permission_classes = [IsOwnerOrReadOnly]
     queryset = Profile.objects.annotate(
-            posts_count=Count('owner__post', distinct=True),
-            followers_count=Count('owner__followed', distinct=True),
-            following_count=Count('owner__following', distinct=True)
-        ).order_by('-created_at')
+        posts_count=Count('owner__post', distinct=True),
+        followers_count=Count('owner__followed', distinct=True),
+        following_count=Count('owner__following', distinct=True)
+    ).order_by('-created_at')
     serializer_class = ProfileSerializer
